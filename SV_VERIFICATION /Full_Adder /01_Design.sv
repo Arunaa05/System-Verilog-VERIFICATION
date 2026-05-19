cd631bd5d1741e22437bd1c2ab6168b94cb5029b
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-19 12:36:38 +0530

    Create 01_Design.sv

---
+++ SV_VERIFICATION /Full_Adder /01_Design.sv
@@ -0,0 +1,13 @@
+module full_adder(
+  input logic a,
+  input logic b,
+  input logic cin,
+  output logic sum,
+  output logic cout
+);
+  always@(*)begin
+    sum=a^b^cin;
+    cout=(a&b)|(b&cin)|(a&cin);
+  end
+  
+endmodule
