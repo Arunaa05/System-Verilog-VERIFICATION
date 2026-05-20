a5bd72bf0d4a1f1d9ddd3d8029c605314e92d833
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 10:16:42 +0530

    Create 10_Output.sv

---
+++ SV_VERIFICATION /D_FlipFlop /Synchronous_Reset /10_Output.sv
@@ -0,0 +1,197 @@
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+time= 10 [driver] rst=1 d=0 q=0
+time=10 [Generator] rst=1 d=0 q=0
+time= 10 [Monitor] rst=0 d=0 q=0
+ACTUAL OUTPUT   : Time = 10 rst=0 d = 0 q = 0
+EXPECTED OUTPUT : Time = 10 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 30 [driver] rst=1 d=0 q=0
+time=30 [Generator] rst=1 d=0 q=0
+time= 30 [Monitor] rst=1 d=0 q=0
+ACTUAL OUTPUT   : Time = 30 rst=1 d = 0 q = 0
+EXPECTED OUTPUT : Time = 30 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 50 [driver] rst=0 d=0 q=0
+time=50 [Generator] rst=0 d=0 q=0
+time= 50 [Monitor] rst=1 d=0 q=0
+ACTUAL OUTPUT   : Time = 50 rst=1 d = 0 q = 0
+EXPECTED OUTPUT : Time = 50 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 70 [driver] rst=1 d=0 q=0
+time=70 [Generator] rst=1 d=0 q=0
+time= 70 [Monitor] rst=0 d=0 q=0
+ACTUAL OUTPUT   : Time = 70 rst=0 d = 0 q = 0
+EXPECTED OUTPUT : Time = 70 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 90 [driver] rst=1 d=1 q=0
+time=90 [Generator] rst=1 d=1 q=0
+time= 90 [Monitor] rst=1 d=0 q=0
+ACTUAL OUTPUT   : Time = 90 rst=1 d = 0 q = 0
+EXPECTED OUTPUT : Time = 90 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 110 [driver] rst=0 d=1 q=0
+time=110 [Generator] rst=0 d=1 q=0
+time= 110 [Monitor] rst=1 d=1 q=0
+ACTUAL OUTPUT   : Time = 110 rst=1 d = 1 q = 0
+EXPECTED OUTPUT : Time = 110 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 130 [driver] rst=0 d=0 q=0
+time=130 [Generator] rst=0 d=0 q=0
+time= 130 [Monitor] rst=0 d=1 q=0
+ACTUAL OUTPUT   : Time = 130 rst=0 d = 1 q = 0
+EXPECTED OUTPUT : Time = 130 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 150 [driver] rst=1 d=1 q=0
+time=150 [Generator] rst=1 d=1 q=0
+time= 150 [Monitor] rst=0 d=0 q=1
+ACTUAL OUTPUT   : Time = 150 rst=0 d = 0 q = 1
+EXPECTED OUTPUT : Time = 150 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 170 [driver] rst=1 d=1 q=0
+time=170 [Generator] rst=1 d=1 q=0
+time= 170 [Monitor] rst=1 d=1 q=0
+ACTUAL OUTPUT   : Time = 170 rst=1 d = 1 q = 0
+EXPECTED OUTPUT : Time = 170 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 190 [driver] rst=1 d=1 q=0
+time=190 [Generator] rst=1 d=1 q=0
+time= 190 [Monitor] rst=1 d=1 q=0
+ACTUAL OUTPUT   : Time = 190 rst=1 d = 1 q = 0
+EXPECTED OUTPUT : Time = 190 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 210 [driver] rst=1 d=0 q=0
+time=210 [Generator] rst=1 d=0 q=0
+time= 210 [Monitor] rst=1 d=1 q=0
+ACTUAL OUTPUT   : Time = 210 rst=1 d = 1 q = 0
+EXPECTED OUTPUT : Time = 210 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 230 [driver] rst=1 d=1 q=0
+time=230 [Generator] rst=1 d=1 q=0
+time= 230 [Monitor] rst=1 d=0 q=0
+ACTUAL OUTPUT   : Time = 230 rst=1 d = 0 q = 0
+EXPECTED OUTPUT : Time = 230 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 250 [driver] rst=1 d=0 q=0
+time=250 [Generator] rst=1 d=0 q=0
+time= 250 [Monitor] rst=1 d=1 q=0
+ACTUAL OUTPUT   : Time = 250 rst=1 d = 1 q = 0
+EXPECTED OUTPUT : Time = 250 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 270 [driver] rst=0 d=0 q=0
+time=270 [Generator] rst=0 d=0 q=0
+time= 270 [Monitor] rst=1 d=0 q=0
+ACTUAL OUTPUT   : Time = 270 rst=1 d = 0 q = 0
+EXPECTED OUTPUT : Time = 270 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 290 [driver] rst=1 d=0 q=0
+time=290 [Generator] rst=1 d=0 q=0
+time= 290 [Monitor] rst=0 d=0 q=0
+ACTUAL OUTPUT   : Time = 290 rst=0 d = 0 q = 0
+EXPECTED OUTPUT : Time = 290 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 310 [driver] rst=1 d=1 q=0
+time=310 [Generator] rst=1 d=1 q=0
+time= 310 [Monitor] rst=1 d=0 q=0
+ACTUAL OUTPUT   : Time = 310 rst=1 d = 0 q = 0
+EXPECTED OUTPUT : Time = 310 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 330 [driver] rst=1 d=1 q=0
+time=330 [Generator] rst=1 d=1 q=0
+time= 330 [Monitor] rst=1 d=1 q=0
+ACTUAL OUTPUT   : Time = 330 rst=1 d = 1 q = 0
+EXPECTED OUTPUT : Time = 330 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 350 [driver] rst=0 d=1 q=0
+time=350 [Generator] rst=0 d=1 q=0
+time= 350 [Monitor] rst=1 d=1 q=0
+ACTUAL OUTPUT   : Time = 350 rst=1 d = 1 q = 0
+EXPECTED OUTPUT : Time = 350 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 370 [driver] rst=0 d=1 q=0
+time=370 [Generator] rst=0 d=1 q=0
+time= 370 [Monitor] rst=0 d=1 q=0
+ACTUAL OUTPUT   : Time = 370 rst=0 d = 1 q = 0
+EXPECTED OUTPUT : Time = 370 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 390 [driver] rst=1 d=0 q=0
+time=390 [Generator] rst=1 d=0 q=0
+time= 390 [Monitor] rst=0 d=1 q=1
+ACTUAL OUTPUT   : Time = 390 rst=0 d = 1 q = 1
+EXPECTED OUTPUT : Time = 390 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+time= 410 [Monitor] rst=1 d=0 q=1
+time= 430 [Monitor] rst=1 d=0 q=0
+time= 450 [Monitor] rst=1 d=0 q=0
+time= 470 [Monitor] rst=1 d=0 q=0
+time= 490 [Monitor] rst=1 d=0 q=0
+time= 510 [Monitor] rst=1 d=0 q=0
+time= 530 [Monitor] rst=1 d=0 q=0
+time= 550 [Monitor] rst=1 d=0 q=0
+time= 570 [Monitor] rst=1 d=0 q=0
+time= 590 [Monitor] rst=1 d=0 q=0
+Simulation complete via $finish(1) at time 1190 NS + 0
+./testbench.sv:31     $finish;
+xcelium> exit
+TOOL:	xrun	25.03-s001: Exiting on May 20, 2026 at 00:08:05 EDT  (total: 00:00:01)
+Done
