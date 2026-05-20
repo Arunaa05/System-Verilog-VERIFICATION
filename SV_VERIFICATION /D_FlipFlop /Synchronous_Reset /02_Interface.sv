6c7cc008cae3966ac1527ef0f586cae32d1d6616
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 09:41:07 +0530

    Create 02_Interface.sv

---
+++ SV_VERIFICATION /D_FlipFlop /Synchronous_Reset /02_Interface.sv
@@ -0,0 +1,29 @@
+interface dff_if;
+  
+  logic d;
+  logic clk;
+  logic rst;
+  logic q;
+  
+  clocking drv_cb@(posedge clk);
+    
+    default input #1 output #0;
+    output d;
+    output rst;
+    input q;
+   
+    
+  endclocking
+  
+  clocking mon_cb@(posedge clk);
+    
+    default input #1step output #0;
+    input d;
+    input rst;
+    input q;
+    
+  endclocking
+    
+  
+endinterface
+  
