0ec2a2d76d02180ca16bef3c9769b5f603495372
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 14:00:32 +0530

    Create 01_Design.sv

---
+++ SV_VERIFICATION /Counter /01_Decade_Counter /Synchronous_Reset /01_Design.sv
@@ -0,0 +1,16 @@
+module decade_counter(
+  input clk,rst,
+  output reg [3:0]q
+);
+  always@(posedge clk)
+  begin 
+    if(rst)
+      q<=4'b0000;
+    else begin
+      if(q>=4'd9)
+        q<=4'b0000;
+      else
+        q<=q+1'b1;
+    end
+  end
+endmodule
