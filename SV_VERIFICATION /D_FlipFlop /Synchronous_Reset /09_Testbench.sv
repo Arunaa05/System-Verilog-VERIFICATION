fdcdf3e07fa05de76ed7dcc520fb9b76d72007b3
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 10:16:06 +0530

    Create 09_Testbench.sv

---
+++ SV_VERIFICATION /D_FlipFlop /Synchronous_Reset /09_Testbench.sv
@@ -0,0 +1,30 @@
+`include "Interface.sv"
+`include "Transaction.sv"
+`include "Generator.sv"
+`include "Driver.sv"
+`include "Monitor.sv"
+`include "Scoreboard.sv"
+`include "Environment.sv"
+
+module tb;
+  dff_if intf();
+  dff dut(
+    .d(intf.d),
+    .clk(intf.clk),
+    .rst(intf.rst),
+    .q(intf.q)
+  );
+  environment env;
+  initial begin
+    intf.clk = 1'b0;
+    forever #10 intf.clk = ~intf.clk;
+  end
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0,tb);
+    env=new(intf);
+    env.run();
+    #800;
+    $finish;
+  end
+endmodule
