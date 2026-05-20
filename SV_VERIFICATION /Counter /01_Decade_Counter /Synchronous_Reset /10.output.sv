96ec28a0453db2dc7308bf8d3037fad2462f9a96
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 14:08:12 +0530

    Create 10.output.sv

---
+++ SV_VERIFICATION /Counter /01_Decade_Counter /Synchronous_Reset /10.output.sv
@@ -0,0 +1,167 @@
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+[Generator] rst=0
+time= 20 [driver] rst=0 
+[Generator] rst=1
+time= 30 [Monitor] rst=0 q=0
+[Scoreboard] Time=30 rst=0 expected_q=0
++------------------+
+|         PASS       |
++------------------+
+time= 40 [driver] rst=1 
+[Generator] rst=0
+time= 50 [Monitor] rst=1 q=1
+[Scoreboard] Time=50 rst=1 expected_q=1
++------------------+
+|         PASS       |
++------------------+
+time= 60 [driver] rst=0 
+[Generator] rst=0
+time= 70 [Monitor] rst=0 q=0
+[Scoreboard] Time=70 rst=0 expected_q=0
++------------------+
+|         PASS       |
++------------------+
+time= 80 [driver] rst=0 
+[Generator] rst=0
+time= 90 [Monitor] rst=0 q=1
+[Scoreboard] Time=90 rst=0 expected_q=1
++------------------+
+|         PASS       |
++------------------+
+time= 100 [driver] rst=0 
+[Generator] rst=0
+time= 110 [Monitor] rst=0 q=2
+[Scoreboard] Time=110 rst=0 expected_q=2
++------------------+
+|         PASS       |
++------------------+
+time= 120 [driver] rst=0 
+[Generator] rst=0
+time= 130 [Monitor] rst=0 q=3
+[Scoreboard] Time=130 rst=0 expected_q=3
++------------------+
+|         PASS       |
++------------------+
+time= 140 [driver] rst=0 
+[Generator] rst=0
+time= 150 [Monitor] rst=0 q=4
+[Scoreboard] Time=150 rst=0 expected_q=4
++------------------+
+|         PASS       |
++------------------+
+time= 160 [driver] rst=0 
+[Generator] rst=0
+time= 170 [Monitor] rst=0 q=5
+[Scoreboard] Time=170 rst=0 expected_q=5
++------------------+
+|         PASS       |
++------------------+
+time= 180 [driver] rst=0 
+[Generator] rst=0
+time= 190 [Monitor] rst=0 q=6
+[Scoreboard] Time=190 rst=0 expected_q=6
++------------------+
+|         PASS       |
++------------------+
+time= 200 [driver] rst=0 
+[Generator] rst=0
+time= 210 [Monitor] rst=0 q=7
+[Scoreboard] Time=210 rst=0 expected_q=7
++------------------+
+|         PASS       |
++------------------+
+time= 220 [driver] rst=0 
+[Generator] rst=0
+time= 230 [Monitor] rst=0 q=8
+[Scoreboard] Time=230 rst=0 expected_q=8
++------------------+
+|         PASS       |
++------------------+
+time= 240 [driver] rst=0 
+[Generator] rst=0
+time= 250 [Monitor] rst=0 q=9
+[Scoreboard] Time=250 rst=0 expected_q=9
++------------------+
+|         PASS       |
++------------------+
+time= 260 [driver] rst=0 
+[Generator] rst=0
+time= 270 [Monitor] rst=0 q=0
+[Scoreboard] Time=270 rst=0 expected_q=0
++------------------+
+|         PASS       |
++------------------+
+time= 280 [driver] rst=0 
+[Generator] rst=0
+time= 290 [Monitor] rst=0 q=1
+[Scoreboard] Time=290 rst=0 expected_q=1
++------------------+
+|         PASS       |
++------------------+
+time= 300 [driver] rst=0 
+[Generator] rst=0
+time= 310 [Monitor] rst=0 q=2
+[Scoreboard] Time=310 rst=0 expected_q=2
++------------------+
+|         PASS       |
++------------------+
+time= 320 [driver] rst=0 
+[Generator] rst=0
+time= 330 [Monitor] rst=0 q=3
+[Scoreboard] Time=330 rst=0 expected_q=3
++------------------+
+|         PASS       |
++------------------+
+time= 340 [driver] rst=0 
+[Generator] rst=1
+time= 350 [Monitor] rst=0 q=4
+[Scoreboard] Time=350 rst=0 expected_q=4
++------------------+
+|         PASS       |
++------------------+
+time= 360 [driver] rst=1 
+[Generator] rst=0
+time= 370 [Monitor] rst=1 q=5
+[Scoreboard] Time=370 rst=1 expected_q=5
++------------------+
+|         PASS       |
++------------------+
+time= 380 [driver] rst=0 
+[Generator] rst=0
+time= 390 [Monitor] rst=0 q=0
+[Scoreboard] Time=390 rst=0 expected_q=0
++------------------+
+|         PASS       |
++------------------+
+time= 400 [driver] rst=0 
+time= 410 [Monitor] rst=0 q=1
+[Scoreboard] Time=410 rst=0 expected_q=1
++------------------+
+|         PASS       |
++------------------+
+time= 430 [Monitor] rst=0 q=2
+[Scoreboard] Time=430 rst=0 expected_q=2
++------------------+
+|         PASS       |
++------------------+
+time= 450 [Monitor] rst=0 q=3
+[Scoreboard] Time=450 rst=0 expected_q=3
++------------------+
+|         PASS       |
++------------------+
+time= 470 [Monitor] rst=0 q=4
+[Scoreboard] Time=470 rst=0 expected_q=4
++------------------+
+|         PASS       |
++------------------+
+time= 490 [Monitor] rst=0 q=5
+[Scoreboard] Time=490 rst=0 expected_q=5
++------------------+
+|         PASS       |
++------------------+
+Simulation complete via $finish(1) at time 500 NS + 0
+./testbench.sv:33         $finish;
+xcelium> exit
+TOOL:	xrun	25.03-s001: Exiting on May 20, 2026 at 04:37:44 EDT  (total: 00:00:01)
+Done
