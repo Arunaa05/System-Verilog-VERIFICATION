de2706146a0c217699b51aecd0f3fc661d58a281
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 10:12:57 +0530

    Create 04_Generator.sv

---
+++ SV_VERIFICATION /D_FlipFlop /Synchronous_Reset /04_Generator.sv
@@ -0,0 +1,28 @@
+class generator;
+  
+  mailbox #(transaction) gen2drv;
+
+  virtual dff_if vif;
+  
+  function new(mailbox #(transaction) gen2drv, virtual dff_if vif);
+    this.gen2drv=gen2drv;
+  
+    this.vif=vif;
+  endfunction
+  
+  task main();
+    transaction t;
+  
+    repeat(20)begin
+      t=new();
+      if (!t.randomize())
+        $display("Randomization failed");
+      gen2drv.put(t);
+     
+      @(vif.mon_cb);
+      t.display("Generator");
+    end
+  endtask
+  
+endclass
+        
