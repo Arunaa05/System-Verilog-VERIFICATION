29d19c6878f9bf7c112ea4a0d352f69f773e1ba5
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 10:13:24 +0530

    Create 05_Driver.sv

---
+++ SV_VERIFICATION /D_FlipFlop /Synchronous_Reset /05_Driver.sv
@@ -0,0 +1,22 @@
+class driver;
+  mailbox #(transaction) gen2drv;
+  virtual dff_if vif;
+  
+  function new(mailbox #(transaction) gen2drv, virtual dff_if vif);
+    this.gen2drv=gen2drv;
+    this.vif=vif;
+  endfunction
+  
+  task main();
+    transaction t;
+    repeat(20)begin
+      gen2drv.get(t);
+      @(vif.drv_cb);
+      vif.drv_cb.d<=t.d;
+      vif.drv_cb.rst<=t.rst;
+     
+      $display("time= %0t [driver] rst=%0d d=%0d q=%0d", $time,t.rst, t.d, t.q);
+    end
+  endtask
+endclass
+  
