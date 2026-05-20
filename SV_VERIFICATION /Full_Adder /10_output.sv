78ce0c18c89b24ede06d14389dd372dd5f47c0c0
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-20 11:33:16 +0530

    Update 10_output.sv

--- SV_VERIFICATION /Full_Adder /10_output.sv
+++ SV_VERIFICATION /Full_Adder /10_output.sv
@@ -1,64 +1,34 @@
-[monitor]a:1 b:1 cin:0 | sum:0 cout:1
-EXPECTED OUTPUT
-sum:0 cout=1
-ACTUAL OUTPUT
-sum:0 cout=1
-+---------+
-|   PASS  |
-+---------+
 [Driver]a:1 b:1 cin:0 | sum:0 cout:0
-[monitor]a:1 b:0 cin:1 | sum:0 cout:1
-EXPECTED OUTPUT
-sum:0 cout=1
-ACTUAL OUTPUT
-sum:0 cout=1
-+---------+
-|   PASS  |
-+---------+
-[monitor]a:1 b:0 cin:1 | sum:0 cout:1
-EXPECTED OUTPUT
-sum:0 cout=1
-ACTUAL OUTPUT
-sum:0 cout=1
-+---------+
-|   PASS  |
-+---------+
 [Driver]a:1 b:0 cin:1 | sum:0 cout:0
-[monitor]a:0 b:1 cin:1 | sum:0 cout:1
-EXPECTED OUTPUT
-sum:0 cout=1
-ACTUAL OUTPUT
-sum:0 cout=1
-+---------+
-|   PASS  |
-+---------+
-[monitor]a:0 b:1 cin:1 | sum:0 cout:1
-EXPECTED OUTPUT
-sum:0 cout=1
-ACTUAL OUTPUT
-sum:0 cout=1
-+---------+
-|   PASS  |
-+---------+
 [Driver]a:0 b:1 cin:1 | sum:0 cout:0
-[monitor]a:0 b:1 cin:0 | sum:0 cout:1
-EXPECTED OUTPUT
-sum:1 cout=0
-ACTUAL OUTPUT
-sum:0 cout=1
-+---------+
-| FAILED  |
-+---------+
-[monitor]a:0 b:1 cin:0 | sum:1 cout:0
-EXPECTED OUTPUT
-sum:1 cout=0
-ACTUAL OUTPUT
-sum:1 cout=0
-+---------+
-|   PASS  |
-+---------+
 [Driver]a:0 b:1 cin:0 | sum:0 cout:0
-[monitor]a:0 b:1 cin:0 | sum:1 cout:0
+[Driver]a:0 b:1 cin:0 | sum:0 cout:0
+[Driver]a:1 b:1 cin:1 | sum:0 cout:0
+[Driver]a:1 b:0 cin:1 | sum:0 cout:0
+[Driver]a:1 b:0 cin:1 | sum:0 cout:0
+[Driver]a:0 b:1 cin:0 | sum:0 cout:0
+[Driver]a:0 b:1 cin:1 | sum:0 cout:0
+[Driver]a:0 b:1 cin:0 | sum:0 cout:0
+[Driver]a:1 b:0 cin:1 | sum:0 cout:0
+[Driver]a:1 b:1 cin:0 | sum:0 cout:0
+[Driver]a:0 b:1 cin:1 | sum:0 cout:0
+[Driver]a:0 b:0 cin:0 | sum:0 cout:0
+[Driver]a:0 b:0 cin:0 | sum:0 cout:0
+[Driver]a:0 b:0 cin:0 | sum:0 cout:0
+[Driver]a:0 b:0 cin:0 | sum:0 cout:0
+[Driver]a:1 b:0 cin:1 | sum:0 cout:0
+[Driver]a:0 b:1 cin:0 | sum:0 cout:0
+[Driver]a:1 b:1 cin:0 | sum:0 cout:0
+[Driver]a:1 b:0 cin:0 | sum:0 cout:0
+[Driver]a:1 b:1 cin:0 | sum:0 cout:0
+[Driver]a:1 b:1 cin:0 | sum:0 cout:0
+[Driver]a:1 b:1 cin:1 | sum:0 cout:0
+[Driver]a:1 b:1 cin:0 | sum:0 cout:0
+[Driver]a:0 b:0 cin:1 | sum:0 cout:0
+[Driver]a:1 b:1 cin:1 | sum:0 cout:0
+[Driver]a:1 b:0 cin:0 | sum:0 cout:0
+[Driver]a:1 b:0 cin:0 | sum:0 cout:0
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
 EXPECTED OUTPUT
 sum:1 cout=0
 ACTUAL OUTPUT
@@ -66,7 +36,7 @@ sum:1 cout=0
 +---------+
 |   PASS  |
 +---------+
-[monitor]a:0 b:1 cin:0 | sum:1 cout:0
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
 EXPECTED OUTPUT
 sum:1 cout=0
 ACTUAL OUTPUT
@@ -74,3 +44,380 @@ sum:1 cout=0
 +---------+
 |   PASS  |
 +---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+[monitor]a:1 b:0 cin:0 | sum:1 cout:0
+EXPECTED OUTPUT
+sum:1 cout=0
+ACTUAL OUTPUT
+sum:1 cout=0
++---------+
+|   PASS  |
++---------+
+
