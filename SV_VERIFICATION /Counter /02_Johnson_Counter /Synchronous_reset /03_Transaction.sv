b5ce8452f0a46c74ad7536fd7eafc7fd6ed95c99
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-22 07:26:55 +0530

    Create 03_Transaction.sv

---
+++ SV_VERIFICATION /Counter /02_Johnson_Counter /Synchronous_reset /03_Transaction.sv
@@ -0,0 +1,13 @@
+class transaction;
+  rand bit rst;
+  bit [3:0]q;
+  
+  
+  function void display(string name);
+    $display("time=%0t [%s] rst=%b q=%b",$time,name,rst,q);
+  endfunction
+  
+  constraint rst_c {rst dist { 1:=20, 0:=80};}
+  
+endclass
+  
